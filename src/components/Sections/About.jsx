b637b9465c081b376ab34b693171d09675e255bf
AngurRWu <AngusWu28074@gmail.com>
2025-12-02 17:40:55 +0800

    Add my own picture

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { useScrollAnimation } from "../../hooks/useScrollAnimation";
+import profilePic from '../../assets/me.png';
+
 
 export default function About() {
   const MotionDiv = useScrollAnimation();
 
   return (
     <MotionDiv className="max-w-2xl mx-auto text-center py-20 px-4">
+      <img
+        src={profilePic}
+        alt="Angus Wu"
+        className="w-64 h-64 rounded-full mx-auto shadow-lg object-cover mb-4"
+      />
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p className="text-lg text-gray-700 dark:text-black-300">
-        I’m AI graduate and software engineer passionate about building beautiful, fast, and meaningful products. 🚀
+        I’m an AI graduate who developed a lung cancer detection and classification model using YOLOv11 and PyTorch. 🚀
       </p>
     </MotionDiv>
   );
